1857a192a293e6014da4cd6e40615d510f9fcbc6
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2021-05-04 14:21:27 -0400

    added DELETE request option

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,17 +3,18 @@ const fs = require('fs');
 const uniqid = require('uniqid');
 const dbData = require('../db/db.json');
 
+let data = dbData;
+
 
 module.exports = (app) => {
   app.get('/api/notes', (req, res) => {
-    res.json(dbData);
+    res.json(data);
   });
 
   app.post('/api/notes', (req, res) => {
     const request = req.body;
     request.id = uniqid();
 
-    const data = dbData;
     data.push(request);
 
     fs.writeFile('./db/db.json', JSON.stringify(data), err => {
@@ -25,14 +26,16 @@ module.exports = (app) => {
     res.json(200);
   });
 
-//   // I added this below code so you could clear out the table while working with the functionality.
-//   // Don"t worry about it!
+  app.delete('/api/notes/:id', (req, res) => {
+    data = data.filter(each => each.id !== req.params.id);
+    console.log(data);
 
-//   app.post('/api/clear', (req, res) => {
-//     // Empty out the arrays of data
-//     tableData.length = 0;
-//     waitListData.length = 0;
-
-//     res.json({ ok: true });
-//   });
+    fs.writeFile('./db/db.json', JSON.stringify(data), err => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    })
+    res.json(data);
+  });
 };
